932ccc9ca0cc90284812add60dac60edf9c5cc01
Chay Auriane <chaya.07@protonmail.com>
2022-09-28 11:41:10 +0200

    feat: multiple markers on map with call to API and plugin leaflet-marker-cluster

---
+++ CNAlpsGeolocateMultiple.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Plugin Name: cnalps-geo multiple
+ */
+
+
+/**
+ * Minimalistic geolocator Shortcode
+ *
+ * @param array $atts Shortcode attributes. Default empty.
+ * @return string Shortcode output.
+ */
+function cnalps_register_geolocate_multiple_shortcode($atts = [])
+{
+    // normalize attribute keys, lowercase
+    $atts = array_change_key_case((array) $atts, CASE_LOWER);
+    $zoom = $atts['zoom'];
+    $id = $atts['id'];
+    $src = $atts['src'];
+
+    //https://mocki.io/v1/f550e470-a665-4cc3-bf0b-22ad935d56b1
+
+    return "
+    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.8.0/leaflet.css\">
+    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.8.0/leaflet.js\"></script>
+
+    <link rel=\"stylesheet\" href=\"/wp-content/plugins/cnalps-geolocator-multiple/Leaflet.markercluster-1.4.1/dist/MarkerCluster.Default.css\">
+    <script src=\"/wp-content/plugins/cnalps-geolocator-multiple/Leaflet.markercluster-1.4.1/dist/leaflet.markercluster.js\"></script>
+
+    <div id=\"cnalps-map-$id\" style=\"width: 300px; height: 300px;\"></div>
+    <script>
+        fetch('$src')
+            .then(response => {
+                return response.json()
+            })
+            .then(response => {
+
+                let map$id = L.map('cnalps-map-$id').fitBounds([[response[0].lat, response[0].lon], [response[1].lat, response[1].lon]], {padding:[50, 50]});
+
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'
+                }).addTo(map$id);
+                var markers = L.markerClusterGroup();
+                response.forEach(marker => {
+                    markers.addLayer(L.marker([marker.lat, marker.lon]).addTo(map$id)
+                    .bindPopup(marker.title)
+                    .openPopup());
+                });
+
+                map$id.addLayer(markers);
+
+            });
+
+        
+    </script>
+    ";
+}
+
+
+add_shortcode('CNAlpsGeolocateMultiple', 'cnalps_register_geolocate_multiple_shortcode');
